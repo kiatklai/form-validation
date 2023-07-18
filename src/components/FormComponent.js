812300b7313406dae592b0658f504c79627cff3c
kiatklai <likhitthaworn.k@gmail.com>
2023-07-18 18:35:54 +0900

    form作成

---
+++ src/components/FormComponent.js
@@ -0,0 +1,31 @@
+const FormComponent =()=>{
+  return (
+    <div className="container">
+      <form className="form">
+        <div className="form-control">
+          <label>Name</label>
+          <input type="text"/>
+          <small>error message</small>
+        </div>
+        <div className="form-control">
+          <label>Email</label>
+          <input type="email"/>
+          <small>error message</small>
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input type="password"/>
+          <small>error message</small>
+        </div>
+        <div className="form-control">
+          <label>Confirmation Password</label>
+          <input type="password"/>
+          <small>error message</small>
+        </div>
+        <button type="submit">Registration</button>
+      </form>
+    </div>
+  )
+}
+
+export default FormComponent

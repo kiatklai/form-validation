f83a5b661696027cdea8a14854d6d66e9e9527df
kiatklai <likhitthaworn.k@gmail.com>
2023-07-19 20:36:21 +0900

    useState

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,28 +1,33 @@
 import './FormComponent.css'
+import {useState} from 'react'
 
 const FormComponent =()=>{
+  const [userName,setUserName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [repassword,setRepassword] = useState('')
   return (
     <div className="container">
       <form className="form">
         <h2>Registration Form</h2>
         <div className="form-control">
           <label>Name</label>
-          <input type="text"/>
+          <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
           <small>error message</small>
         </div>
         <div className="form-control">
           <label>Email</label>
-          <input type="email"/>
+          <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
           <small>error message</small>
         </div>
         <div className="form-control">
           <label>Password</label>
-          <input type="password"/>
+          <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
           <small>error message</small>
         </div>
         <div className="form-control">
           <label>Confirmation Password</label>
-          <input type="password"/>
+          <input type="password" value={repassword} onChange={(e)=>setRepassword(e.target.value)}/>
           <small>error message</small>
         </div>
         <button type="submit">Registration</button>

0ef44fc4f0097ed5dfab8ff8c0fb99c850e5f253
kiatklai <likhitthaworn.k@gmail.com>
2023-07-20 16:49:05 +0900

    エラーメッセージ

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -6,6 +6,12 @@ const FormComponent =()=>{
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const [repassword,setRepassword] = useState('')
+
+  const [errorUserName,serErrorUserName] = useState('Please insert more than 8 characters')
+  const [errorEmail,setErrorEmail] = useState('Email is incorrect')
+  const [errorPassword,setErrorPassword] = useState('Password must more than 8 characters')
+  const [errorRePassword,setErrorRePassword] = useState('Comfirmation Password is not match password')
+
   return (
     <div className="container">
       <form className="form">
@@ -13,22 +19,22 @@ const FormComponent =()=>{
         <div className="form-control">
           <label>Name</label>
           <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
-          <small>error message</small>
+          <small>{errorUserName}</small>
         </div>
         <div className="form-control">
           <label>Email</label>
           <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-          <small>error message</small>
+          <small>{errorEmail}</small>
         </div>
         <div className="form-control">
           <label>Password</label>
           <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-          <small>error message</small>
+          <small>{errorPassword}</small>
         </div>
         <div className="form-control">
           <label>Confirmation Password</label>
           <input type="password" value={repassword} onChange={(e)=>setRepassword(e.target.value)}/>
-          <small>error message</small>
+          <small>{errorRePassword}</small>
         </div>
         <button type="submit">Registration</button>
       </form>
